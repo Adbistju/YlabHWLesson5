6cb17b6aa87cfa71a1d52a66e3ac2ae048ad452b
Adbistju <adjytant@gmail.com>
2021-11-17 13:49:08 +0300

    add adstract Root

--- src/main/java/Adbistju/system/SaxParserHandler.java
+++ src/main/java/Adbistju/system/SaxParserHandler.java
@@ -4,12 +4,9 @@ import Adbistju.system.comparator.Comparator;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.ArrayList;
-
 public class SaxParserHandler extends DefaultHandler {
-    private static ArrayList<String> address = new ArrayList<>();
 
-    private boolean currentIsFile = false;
+
     private String currentTagName;
 
     private Comparator comparator;
@@ -22,15 +19,15 @@ public class SaxParserHandler extends DefaultHandler {
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
         currentTagName = qName;
         if(currentTagName.equals(Constant.child)){
-            currentIsFile = Boolean.parseBoolean(attributes.getValue(Constant.isFile));
+            comparator.setCurrentIsFile(Boolean.parseBoolean(attributes.getValue(Constant.isFile)));
         }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) {
         if(currentTagName.equals(Constant.child)){
-            currentIsFile = false;
-            address.remove(address.size()-1);
+            comparator.setCurrentIsFile(false);
+            comparator.exitDir();
         }
         currentTagName = qName;
     }
@@ -41,29 +38,22 @@ public class SaxParserHandler extends DefaultHandler {
             return;
         }
         if(!new String(ch, start, length).isBlank()){
-            if(address.size() == 0){
+            if(comparator.getAddress().size() == 0){
                 if (new String(ch, start, length).equals(Constant.delimiter)){
-                    address.add("");
+                    comparator.addAddress("");
                 }else{
-                    address.add(new String(ch, start, length));
+                    comparator.addAddress(new String(ch, start, length));
                 }
             }else if(currentTagName.equals(Constant.name)){
-                if(currentIsFile && comparator.check(new String(ch, start, length))){
-                    print(new String(ch, start, length));
-                }else if(!currentIsFile && comparator.check(new String(ch, start, length))){
-                    print(new String(ch, start, length));
-                    address.add(new String(ch, start, length));
-                }else if(!currentIsFile){
-                    address.add(new String(ch, start, length));
+                if(comparator.isCurrentIsFile() && comparator.check(new String(ch, start, length))){
+                    comparator.print(new String(ch, start, length));
+                }else if(!comparator.isCurrentIsFile() && comparator.check(new String(ch, start, length))){
+                    comparator.print(new String(ch, start, length));
+                    comparator.addAddress(new String(ch, start, length));
+                }else if(!comparator.isCurrentIsFile()){
+                    comparator.addAddress(new String(ch, start, length));
                 }
             }
         }
-    }
-
-    private void print(String str){
-        for (int i = 0; i < address.size(); i++) {
-            System.out.print(address.get(i)+Constant.delimiter);
-        }
-        System.out.println(str);
     }
 }
